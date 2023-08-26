a6de0f4ca6288a0357447c2a3c1b775b8b581382
Alberto Arroyo Raygada <beyondnet.peru@gmail.com>
2023-08-26 18:31:34 -0500

    adding samples

--- imdb/src/app/layout.jsx
+++ imdb/src/app/layout.jsx
@@ -1,17 +1,28 @@
+import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
+import SearchBox from "@/components/SearchBox";
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "IMDB Clone",
-  description: "IMDB Clone",
-};
+import Providers from "./Providers";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Providers>
+          {/* Header */}
+          <Header />
+
+          {/* Navbar */}
+
+          <Navbar />
+
+          {/* SearchBox */}
+
+          <SearchBox />
+
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
